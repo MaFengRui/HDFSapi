9c1de60feb3a07fa8179f75bda6e5a4383e263ae
MaFengRui <mfr8520@163.com>
2018-09-14 23:32:21 +0800

    hdfsAPI

--- HdfsTest.java
+++ HdfsTest.java
@@ -3,24 +3,27 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
 public class HdfsTest {
-     FileSystem fileSystem = null;
-     public   void HdfsConnection()throws Exception{
+    FileSystem fileSystem = null;
+
+
+     //
+     public    void HdfsConnection()throws Exception{
          Configuration con = new Configuration();
          fileSystem = FileSystem.newInstance(con);
 
          fileSystem.mkdirs(new Path("/dd"));
-         System.out.print(4444);
-         System.out.print(3);
+
          fileSystem.close();
      }
 
      public void TestPut(String inpath,String outpath){
-
+         
 
      }
 
 
     public static void main(String[] args) throws Exception{
+
 
 
     }
